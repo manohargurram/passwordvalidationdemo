a848f3b3902e62fa759e23bf8efd08feec0c8911
ACER <ACER@E1-570G>
2021-06-26 11:01:43 +0530

    added testcase for password length

--- src/test/java/com/myapp/service/PasswordValidationServiceTest.java
+++ src/test/java/com/myapp/service/PasswordValidationServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,24 +17,42 @@ import com.myapp.vo.PasswordSuccessResponseVO;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PasswordValidationServiceTest {
-	
+
+	private static Logger logger = LoggerFactory.getLogger(PasswordValidationServiceTest.class);
+
 	@Autowired private PasswordValidationService passwordValidationService;
 
 	@Test
 	public void validateCorrectPassword() {
+		logger.info("validating valid password");
 		PasswordRequestVO passwordRequestVO = new PasswordRequestVO("1aA222222222");
 		PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
 		assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
 	}
-	
+
 	@Test
 	public void validateNullPassword() {
 		try {
-		PasswordRequestVO passwordRequestVO = new PasswordRequestVO(null);
-		PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
-		assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
+			logger.info("validating null password");
+			PasswordRequestVO passwordRequestVO = new PasswordRequestVO(null);
+			PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
+			assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
 		} catch (ValidationException ex) {
+			logger.info("exception response:"+ex.getErrorsList().toString());
 			assertEquals("password should not be null", ex.getErrorsList().get(0).getErrorMessage());
+		}
+	}
+
+	@Test
+	public void validatePasswordLength() {
+		try {
+			logger.info("validating  password length");
+			PasswordRequestVO passwordRequestVO = new PasswordRequestVO("111");
+			PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
+			assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
+		} catch (ValidationException ex) {
+			logger.info("exception response:"+ex.getErrorsList().toString());
+			assertTrue(true);
 		}
 	}
 
