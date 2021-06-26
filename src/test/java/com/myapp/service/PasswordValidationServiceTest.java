561a3db1aa0f4a3dfb8af9ce6465851d6b07ec82
ACER <ACER@E1-570G>
2021-06-26 10:52:35 +0530

    added testcase for null password

--- src/test/java/com/myapp/service/PasswordValidationServiceTest.java
+++ src/test/java/com/myapp/service/PasswordValidationServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.myapp.exception.ValidationException;
 import com.myapp.vo.PasswordRequestVO;
 import com.myapp.vo.PasswordSuccessResponseVO;
 
@@ -22,6 +23,17 @@ public class PasswordValidationServiceTest {
 		PasswordRequestVO passwordRequestVO = new PasswordRequestVO("1aA222222222");
 		PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
 		assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
+	}
+	
+	@Test
+	public void validateNullPassword() {
+		try {
+		PasswordRequestVO passwordRequestVO = new PasswordRequestVO(null);
+		PasswordSuccessResponseVO responseVO = passwordValidationService.validate(passwordRequestVO);
+		assertEquals(new PasswordSuccessResponseVO("valid password"), responseVO);
+		} catch (ValidationException ex) {
+			assertEquals("password should not be null", ex.getErrorsList().get(0).getErrorMessage());
+		}
 	}
 
 }
